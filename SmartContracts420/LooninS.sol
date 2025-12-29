4b88a10b33556f4e6e462c1e3c56a27aab31092f
LooninS <hushed55@proton.me>
2025-12-30 02:26:28 +0530

    made changes to smart contract and removed unnessacary func

--- SmartContracts420/LooninS.sol
+++ SmartContracts420/LooninS.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -7,34 +6,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract HeroOfTheVillage is ERC721, Ownable {
     uint256 private _nextTokenId;
     
-    mapping(address => uint256) public nftBalances;
-
     constructor() ERC721("Hero of the Village", "HOTV") Ownable(msg.sender) {}
 
     function deposit(address to, uint256 tokenId) external onlyOwner {
-        _mint(to, tokenId);
-        nftBalances[to]++;
+        require(tokenId == 0 || tokenId == _nextTokenId, "Use 0 for sequential or exact next ID");
+        uint256 mintId = tokenId == 0 ? _nextTokenId++ : tokenId;
+        _mint(to, mintId);
     }
 
     function withdraw(uint256 tokenId) external {
         require(ownerOf(tokenId) == msg.sender, "Not the owner");
         _burn(tokenId);
-        nftBalances[msg.sender]--;
     }
 
     function getBalance(address owner) external view returns (uint256) {
         return balanceOf(owner);
-    }
-
-    function _update(address to, uint256 tokenId, address auth) internal override returns (address) {
-        address from = _ownerOf(tokenId);
-        nftBalances[from]--;
-        nftBalances[to]++;
-        return super._update(to, tokenId, auth);
-    }
-
-    function _increaseBalance(address account, uint128 value) internal virtual override {
-        super._increaseBalance(account, value);
     }
 }
 
