703638516f9239bc822ad08f0e12f1a63590e13c
yogesh4216 <yogesh2005m@gmail.com>
2026-01-05 20:01:15 +0530

    Fix Ownable constructor for HeroOfTheVillage ERC721

--- SmartContracts420/yogesh4216.sol
+++ SmartContracts420/yogesh4216.sol
@@ -8,6 +8,31 @@ contract HeroOfTheVillage is ERC721, Ownable {
 
     constructor() ERC721("HeroOfTheVillage", "HERO") {}
 
+    // Mint NFT (Deposit)
+    function deposit(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    // Withdraw NFT back to contract owner
+    function withdraw(uint256 tokenId) public {
+        require(ownerOf(tokenId) == msg.sender, "Not token owner");
+        _transfer(msg.sender, owner(), tokenId);
+    }
+
+    // Check NFT balance
+    function getBalance(address ownerAddress) public view returns (uint256) {
+        return balanceOf(ownerAddress);
+    }
+}// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract HeroOfTheVillage is ERC721, Ownable {
+
+    constructor() ERC721("HeroOfTheVillage", "HERO") {}
+
     // Mint (Deposit) NFT
     function deposit(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
